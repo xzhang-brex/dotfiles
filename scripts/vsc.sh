5a0327cb8a1911274f0ed74aeae2e40a8bbbce54
Sridhar Mocherla <smocherla@brex.com>
2023-10-20 04:16:09 +0000

    Fix VSCode script

--- scripts/vsc.sh
+++ scripts/vsc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y code-server
+curl -fsSL https://code-server.dev/install.sh | sh
 
 # Download extensions
 wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/HashiCorp/vsextensions/HCL/0.3.2/vspackage
 wget https://github.com/golang/vscode-go/releases/download/v0.39.1/go-0.39.1.vsix
 wget https://github.com/bazelbuild/vscode-bazel/releases/download/0.7.0/vscode-bazel-0.7.0.vsix
 
-code-server --install-extension go-0.39.1.vsix
-code-server --install-extension vscode-bazel-0.7.0.vsix
+code-server --extensions-dir ~/.vscode-server/extensions --install-extension go-0.39.1.vsix
+code-server --extensions-dir ~/.vscode-server/extensions --install-extension vscode-bazel-0.7.0.vsix
