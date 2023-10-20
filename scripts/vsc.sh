c89225b64d7d95fdb37f66696e9a8960746f27df
Sridhar Mocherla <smocherla@brex.com>
2023-10-20 04:17:42 +0000

    Remove unnecessary

--- scripts/vsc.sh
+++ scripts/vsc.sh
@@ -3,7 +3,6 @@
 curl -fsSL https://code-server.dev/install.sh | sh
 
 # Download extensions
-wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/HashiCorp/vsextensions/HCL/0.3.2/vspackage
 wget https://github.com/golang/vscode-go/releases/download/v0.39.1/go-0.39.1.vsix
 wget https://github.com/bazelbuild/vscode-bazel/releases/download/0.7.0/vscode-bazel-0.7.0.vsix
 
