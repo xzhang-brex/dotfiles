29ba67056a56d133a4ecd6385d5a23c2954c9c0f
Sridhar Mocherla <smocherla@brex.com>
2023-10-25 01:53:31 -0400

    Update intellij.sh

--- scripts/intellij.sh
+++ scripts/intellij.sh
@@ -3,4 +3,4 @@
 # Install some of my custom IntelliJ plugins
 BREX_INTELLIJ_VERSION=${BREX_INTELLIJ_VERSION:-2023.2.2}
 
-~/.cache/JetBrains/RemoteDev/dist/ideaIU-${BREX_INTELLIJ_VERSION}/bin/remote-dev-server.sh installPlugins /home/owner/src/credit_card zielu.gittoolbox Pythonid
+REMOTE_DEV_NON_INTERACTIVE=1 ~/.cache/JetBrains/RemoteDev/dist/ideaIU-${BREX_INTELLIJ_VERSION}/bin/remote-dev-server.sh installPlugins /home/owner/src/credit_card zielu.gittoolbox Pythonid
