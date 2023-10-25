b8375fa53c8c90c2529a1c96c286d7391b52adc6
Sridhar Mocherla <smocherla@brex.com>
2023-10-25 12:03:41 -0400

    Delete scripts/vundle.sh

--- scripts/vundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "📦 Installing Vundle"
-
-mkdir -p $HOME/.vim/bundle
-pushd $HOME/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git
-vim +PluginInstall +qall
-popd
