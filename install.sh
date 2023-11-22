dee07a405f2e576ff77f03ae2ab6ac1c29058039
Xu Zhang <xzhang@brex.com>
2023-11-22 08:44:55 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ scripts/vsc.sh
 echo "Installing some IntelliJ plugins..."
 scripts/intellij.sh
 
-echo "Importing IDE settings for XU..."
-mkdir -p ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
-unzip settings.zip -d ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
+# echo "Importing IDE settings for XU..."
+# mkdir -p ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
+# unzip settings.zip -d ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
 
